8bc374f9e88c4596b485347b7d26d3779a6e7e6c
Stuart Stein <qmstuart@gmail.com>
2024-08-13 21:39:12 +0100

    test event

--- manager/manager.go
+++ manager/manager.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 )
 
 type Event struct {
 	Id          int
 	Description string
-	StartDate   string
+	Date        string
 	Type        string
 	Location    string
 }
@@ -15,13 +17,12 @@ type Event struct {
 func main() {
 	fmt.Println("Calendar manager")
 
-	// read the json file from repo url
+	events := loadJson()
 
-	// parse the json file
+	fmt.Printf("Description: %s\n", events[0].Description)
 
-	// show the menu
 	for {
-		fmt.Print("\033[H\033[2J")
+		// /fmt.Print("\033[H\033[2J")
 		showMenu()
 		var input int
 		fmt.Scanln(&input)
@@ -48,4 +49,20 @@ func showMenu() {
 	fmt.Println("3. Delete event")
 	fmt.Println("4. View event")
 	fmt.Println("5. Quit")
+}
+
+func loadJson() []Event {
+	// read the json file from repo url
+	url := "https://github.com/stuartstein777/calendar/blob/main/events.json"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error reading the json file")
+	}
+	defer resp.Body.Close()
+
+	event := []Event{}
+	json.NewDecoder(resp.Body).Decode(&event)
+	return event
+
 }
